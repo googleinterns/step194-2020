bf6b0b22ae882911549e4fadae92485b8d473298
Caroline <carolineehana@gmail.com>
2020-06-24 18:22:27 +0000

    Add CarolineHana submodule

---
+++ Hana-projects/README.md
@@ -0,0 +1,5 @@
+# Welcome!
+
+This repo contains Caroline's portfolio and STEP projects.
+
+This is part of Google's Student Training in Engineering Program Summer 2020.
---
+++ Hana-projects/portfolio/src/main/webapp/script.js
@@ -0,0 +1,32 @@
+ 
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Adds a random greeting to the page.
+ */
+function addRandomFunFacts() {
+  const FunFacts =
+      ['I can do a hand stand!', 'I can touch my nose with my tongue!', 'I was on the wrestling team for 4 years!', 'I can speak Arabic!'];
+
+  // Pick a random greeting.
+  const facts = FunFacts[Math.floor(Math.random() * FunFacts.length)];
+
+  // Add it to the page.
+  const factsContainer = document.getElementById('facts-container');
+  factsContainer.innerText = facts;
+}
+
+
+

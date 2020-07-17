5d3e5c56c9ecceb14c6ff9f6eb4f20710bde1271
Noah Krohngold <noah.krohngold@gmail.com>
2020-07-17 13:35:03 -0700

    Change Video fields and packages for servlets

--- src/main/java/com/google/step/YTLounge/data/Video.java
+++ src/main/java/com/google/step/YTLounge/data/Video.java
@@ -3,14 +3,14 @@ package com.google.step.YTLounge.data;
 /** A YouTube video's data and relevant information */
 public class Video {
   private final String title;
-  private final String thumbnailLink;
-  private final String videoLink;
+  private final String thumbnailURL;
+  private final String videoURL;
   private final long duration;
 
-  public Video(String title, String thumbnailLink, String videoLink, long duration) {
+  public Video(String title, String thumbnailURL, String videoURL, long duration) {
     this.title = title;
-    this.thumbnailLink = thumbnailLink;
-    this.videoLink = videoLink;
+    this.thumbnailURL = thumbnailURL;
+    this.videoURL = videoURL;
     this.duration = duration;
   }
 }

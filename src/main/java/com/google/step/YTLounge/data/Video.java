9c47743921fb96fa5e1cfa19a4f021fa691c005d
Noah Krohngold <noah.krohngold@gmail.com>
2020-07-17 11:26:48 -0700

    Fix whitespace issue

--- src/main/java/com/google/step/YTLounge/data/Video.java
+++ src/main/java/com/google/step/YTLounge/data/Video.java
@@ -3,16 +3,15 @@ package com.google.step.YTLounge.data;
 /** A YouTube video's data and relevant information */
 
 public class Video {
-    private final String title;
-    private final String thumbnailLink;
-    private final String videoLink;
-    private final long duration;
+  private final String title;
+  private final String thumbnailLink;
+  private final String videoLink;
+  private final long duration;
 
-    public Video (String title, String thumbnailLink, String videoLink,
-        long duration) {
-        this.title = title;
-        this.thumbnailLink = thumbnailLink;
-        this.videoLink = videoLink;
-        this.duration = duration;
-    }
+  public Video(String title, String thumbnailLink, String videoLink, long duration) {
+    this.title = title;
+    this.thumbnailLink = thumbnailLink;
+    this.videoLink = videoLink;
+    this.duration = duration;
+  }
 }

99aaba19b651940b6344f8fcc0ae4391d9eb890f
Noah Krohngold <noah.krohngold@gmail.com>
2020-07-17 11:28:42 -0700

    Remove unnecessary empty line

--- src/main/java/com/google/step/YTLounge/data/Video.java
+++ src/main/java/com/google/step/YTLounge/data/Video.java
@@ -1,7 +1,6 @@
 package com.google.step.YTLounge.data;
 
 /** A YouTube video's data and relevant information */
-
 public class Video {
   private final String title;
   private final String thumbnailLink;

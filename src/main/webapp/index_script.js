4bdd69c98200004647a11226692ca47602ca3dfe
Caroline <carolineehana@gmail.com>
2020-07-21 21:42:30 +0000

    Linter fixes.

--- src/main/webapp/index_script.js
+++ src/main/webapp/index_script.js
@@ -1,15 +1,15 @@
 // opens chat and queue on the side and adjusts content size on screen
-function openSidebar() {
+function openSidebar() { // eslint-disable-line no-unused-vars
   const buttonText = document.getElementById('sidebarBtn');
 
-  if (buttonText.innerHTML ===  
+  if (buttonText.innerHTML ===
     '<span class="glyphicon glyphicon-menu-left"></span>') {
-    buttonText.innerHTML = 
+    buttonText.innerHTML =
     '<span class="glyphicon glyphicon-menu-right"></span>';
     document.getElementById('mySidebar').style.width = '25%';
     document.getElementById('main').style.marginRight = '25%';
   } else {
-    buttonText.innerHTML = 
+    buttonText.innerHTML =
     '<span class="glyphicon glyphicon-menu-left"></span>';
     document.getElementById('mySidebar').style.width = '0';
     document.getElementById('main').style.marginRight= '0';

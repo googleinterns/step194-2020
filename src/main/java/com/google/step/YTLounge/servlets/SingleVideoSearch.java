4ed9a811a4bcc2c4f95bc02e127aadf84f59b5a9
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-07-22 17:30:05 +0000

    Linter fixes

--- src/main/java/com/google/step/YTLounge/servlets/SingleVideoSearch.java
+++ src/main/java/com/google/step/YTLounge/servlets/SingleVideoSearch.java
@@ -18,20 +18,11 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreService.*;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.*;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
-
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.VideoListResponse;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -42,7 +33,6 @@ import java.lang.Integer;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -52,38 +42,37 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/vSearch")
 public class SingleVideoSearch extends HttpServlet {
 
-  private static final String CLIENT_SECRETS= "~/step194-2020/src/main/java/com/google/step/YTLoung/servlets/client_secret.json";
+  private static final String CLIENT_SECRETS= 
+    "~/step194-2020/src/main/java/com/google/step/YTLoung/servlets/client_secret.json";
   private static final Collection<String> SCOPES =
       Arrays.asList("https://www.googleapis.com/auth/youtube.readonly");
-
   private static final String APPLICATION_NAME = "YouTube Lounge";
   private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
   /**
-    * Build and return an authorized API client service.
-    *
-    * @return an authorized API client service
-    * @throws GeneralSecurityException, IOException
-    */
+   * Build and return an authorized API client service.
+   *
+   * @return an authorized API client service
+   * @throws GeneralSecurityException, IOException
+   */
   public YouTube getService() throws GeneralSecurityException, IOException {
-      final YouTubeRequestInitializer KEY_INITIALIZER =
+    final YouTubeRequestInitializer KEY_INITIALIZER =
       new YouTubeRequestInitializer("AIzaSyBIwlgpsYZLoI7cr347HyBoAETX9FvzXps");
-      final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-      return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
-          .setApplicationName("YouTube Lounge")
-          .setYouTubeRequestInitializer(KEY_INITIALIZER)
-          .build();
+    final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
+      .setApplicationName("YouTube Lounge")
+      .setYouTubeRequestInitializer(KEY_INITIALIZER)
+      .build();
   }
 
   /**
-    * Retrieve query parameters, extract the video if video exists, and 
-    * create new Lounge if necessary based on query request.
-    * Prints video response when complete.
-    * @throws IOException
-    */
+   * Retrieve query parameters, extract the video if video exists, and create new Lounge if
+   * necessary based on query request. Prints video response when complete.
+   *
+   * @throws IOException
+   */
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-    throws IOException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     //accesses YouTube API to get a specific video as long as the given URL is valid
     Gson gson = new Gson();
     response.setContentType("application/json");
@@ -99,12 +88,11 @@ public class SingleVideoSearch extends HttpServlet {
     if (youtubeService == null) {
       return;
     }
-    YouTube.Videos.List videoRequest = youtubeService.videos()
-      .list("snippet,contentDetails");
+    YouTube.Videos.List videoRequest = youtubeService.videos().list("snippet,contentDetails");
     VideoListResponse videoResponse = videoRequest.setId(videoID).execute();
     JsonObject jsonObject = new JsonParser().parse(gson.toJson(videoResponse)).getAsJsonObject();
     JsonObject error = jsonObject.getAsJsonObject("error");
-    if (error != null) { //if video doesn't exist
+    if (error != null) { // if video doesn't exist
       return;
     }
 
@@ -113,38 +101,44 @@ public class SingleVideoSearch extends HttpServlet {
   }
 
   /**
-    * If a room wasn't found from the query, create a new room ID
-    * while respecting the current IDs in DataStore. Initializes
-    * necessary properties for the room.
-    * @return a string representing a room's identifier
-    */
+   * If a room wasn't found from the query, create a new room ID while respecting the current IDs in
+   * DataStore. Initializes necessary properties for the room.
+   *
+   * @return a string representing a room's identifier
+   */
   private String generateRoomID() {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Entity roomEntity = new Entity("room");
-      roomEntity.setProperty("members", new HashSet<Key>());
-      roomEntity.setProperty("nowPlaying", "");
-      roomEntity.setProperty("queue", new ArrayList<Key>());
-      roomEntity.setProperty("duration", 0);
-      roomEntity.setProperty("elapsedTime", 0);
-      roomEntity.setProperty("log", new ArrayList<Key>());
+    roomEntity.setProperty("members", new HashSet<Key>());
+    roomEntity.setProperty("nowPlaying", "");
+    roomEntity.setProperty("queue", new ArrayList<Key>());
+    roomEntity.setProperty("duration", 0);
+    roomEntity.setProperty("elapsedTime", 0);
+    roomEntity.setProperty("log", new ArrayList<Key>());
     datastore.put(roomEntity);
     return Long.toString(roomEntity.getKey().getId());
   }
 
   /**
-    * Iterates through the given items and locates specific values to 
-    * create a new Video and upload the video to DataStore
-    */
+   * Iterates through the given items and locates specific values to 
+   * create a new Video and upload the video to DataStore
+   */
   private void extractVideo(JsonArray items, String videoID) {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Entity videoEntity = new Entity("Video");
     for (int i = 0; i < items.size(); i++) {
       JsonObject snippet = items.get(i).getAsJsonObject().getAsJsonObject("snippet");
       String title = snippet.get("title").toString();
-      String thumbnailURL = snippet.getAsJsonObject("thumbnails").getAsJsonObject("medium").get(
-        "url").toString();
+      String thumbnailURL = 
+          snippet.getAsJsonObject("thumbnails").getAsJsonObject("medium").get("url").toString();
       String formattedVideoURL = "https://youtube.com/watch?v=" + videoID;
-      String duration = items.get(i).getAsJsonObject().getAsJsonObject("contentDetails").get("duration").toString(); //null pointer exception!!!
+      String duration = 
+        items
+            .get(i)
+            .getAsJsonObject()
+            .getAsJsonObject("contentDetails")
+            .get("duration")
+            .toString();
       String channelName = snippet.get("channelTitle").toString();
       String releaseDate = snippet.get("publishedAt").toString();
       
@@ -156,26 +150,25 @@ public class SingleVideoSearch extends HttpServlet {
       videoEntity.setProperty("channelName", channelName);
       videoEntity.setProperty("releaseDate", releaseDate);
       videoEntity.setProperty("requestTime", System.currentTimeMillis());
-      datastore.put(videoEntity); //place video in queue for that room
+      datastore.put(videoEntity); // place video in queue for that room
     }
   }
 
   /**
-    * Takes the given string and converts it into pure seconds.
-    * String must be either in the format of "PT#H#M#S" or
-    * "PT#M#S" to be properly parsed.
-    *
-    * @return a long for the number of seconds in encodedTime
-    */
+   * Takes the given string and converts it into pure seconds. String must be either in the format
+   * of "PT#H#M#S" or "PT#M#S" to be properly parsed.
+   *
+   * @return a long for the number of seconds in encodedTime
+   */
   private long parseDuration(String encodedTime) {
     String shortenedTime = encodedTime.substring(2);
     long hours = 0;
     long minutes = 0;
     long seconds = 0;
     if (shortenedTime.contains("H")) {
-        hours = Integer.parseInt(shortenedTime.substring(0, shortenedTime.indexOf("H")));
-        seconds += (hours * 3600);
-        shortenedTime = shortenedTime.substring(shortenedTime.indexOf("H") + 1);
+      hours = Integer.parseInt(shortenedTime.substring(0, shortenedTime.indexOf("H")));
+      seconds += (hours * 3600);
+      shortenedTime = shortenedTime.substring(shortenedTime.indexOf("H") + 1);
     }
     minutes = Integer.parseInt(shortenedTime.substring(0, shortenedTime.indexOf("M")));
     seconds += (minutes * 60);
@@ -186,12 +179,12 @@ public class SingleVideoSearch extends HttpServlet {
   }
 
   /**
-    * Locates the name parameter in the given request and
-    * returns that value.
-    *
-    * @return defaultValue if name parameter wasn't found
-    * @return found value for name parameter
-    */
+   * Locates the name parameter in the given request and
+   * returns that value.
+   *
+   * @return defaultValue if name parameter wasn't found
+   * @return found value for name parameter
+   */
   private String getParameter(HttpServletRequest request, String name, String defaultValue) {
     String value = request.getParameter(name);
     if (value == null) {
--- src/main/webapp/js/queue.js
+++ src/main/webapp/js/queue.js
@@ -3,50 +3,46 @@
   if their link wasn't formatted correctly for the server to parse.
 */
 function verifyURLStructure(url) {
-  if (!url.includes("https://www.youtube.com/watch?v=") || url.length <= 32) {
-    console.log("false"); //signal bad url to user
-  }
-  else if (document.getElementById("videoplayer") != null) { //already in a room
-    console.log("good structure!");
-    getVideoData(document.getElementById("linkArea").value.substring(32));
-  }
-  else { //starting a room
-    console.log("good structure!");
-    getVideoData(document.getElementById("linkArea").value.substring(32));
+  if (!url.includes('https://www.youtube.com/watch?v=') || url.length <= 32) {
+    console.log('false'); // signal bad url to user
+  } else if (document.getElementById('videoplayer') != null) { // already in a room
+    console.log('good structure!');
+    getVideoData(document.getElementById('linkArea').value.substring(32));
+  } else { // starting a room
+    console.log('good structure!');
+    getVideoData(document.getElementById('linkArea').value.substring(32));
   }
 }
 
-//Retrieve video and create a new room via servlet GET request
+// Retrieve video and create a new room via servlet GET request
 async function getVideoData(id) {
   if (id == "") {
     return;
   }
-  fetch("/vSearch?id=" + id)
-  .then(response => response.json())
-  .then((video) => {
-    if (video.error != null) { //video was found, now load it into the website
-      //put thumbnail, title, duration, and channel name onto page
-      console.log(video);
-    }
-    else { //video wasn't found, signal error to user
-      console.log(video.error);
+  fetch('/vSearch?id=' + id)
+      .then((response) => response.json())
+      .then((video) => {
+            if (video.error != null) { // video was found, now load it into the website
+            // put thumbnail, title, duration, and channel name onto page
+            console.log(video);
+            } else { // video wasn't found, signal error to user
+              console.log(video.error);
     }
   });
 }
 
-//Within a room, so just add video to room's queue via room ID
+// Within a room, so just add video to room's queue via room ID
 async function getVideoData(id, room) {
-  if (id == "" || room == "") {
+  if (id == '' || room == '') {
     return;
   }
-  fetch("/vSearch?id=" + id + "&room=" + room)
-  .then(response => response.json())
+  fetch('/vSearch?id=' + id + '&room=' + room)
+  .then((response) => response.json())
   .then((video) => {
-    if (video.error != null) { //video was found, now load it into the website
-      //put thumbnail, title, duration, and channel name onto page
+    if (video.error != null) { // video was found, now load it into the website
+      // put thumbnail, title, duration, and channel name onto page
       console.log(video);
-    }
-    else { //video wasn't found, signal error to user
+    } else { // video wasn't found, signal error to user
       console.log(video.error);
     }
   });

84fc204dce911bdd7cbd77d38823318e3edb547e
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-07-22 17:50:57 +0000

    More linter fixes

--- src/main/java/com/google/step/YTLounge/servlets/SingleVideoSearch.java
+++ src/main/java/com/google/step/YTLounge/servlets/SingleVideoSearch.java
@@ -1,18 +1,10 @@
 package com.google.step.YTLounge.servlets;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.googleapis.services.CommonGoogleClientRequestInitializer;
-import com.google.api.client.googleapis.services.json.CommonGoogleJsonClientRequestInitializer;
-import com.google.api.services.youtube.YouTubeRequestInitializer;
+import com.google.api.services.youtube.YouTube;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -21,17 +13,12 @@ import com.google.gson.JsonParser;
 import com.google.appengine.api.datastore.*;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
-import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.VideoListResponse;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.lang.Integer;
 import java.util.HashSet;
-import java.util.List;
 import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -43,7 +30,7 @@ import javax.servlet.http.HttpServletResponse;
 public class SingleVideoSearch extends HttpServlet {
 
   private static final String CLIENT_SECRETS= 
-    "~/step194-2020/src/main/java/com/google/step/YTLoung/servlets/client_secret.json";
+      "~/step194-2020/src/main/java/com/google/step/YTLoung/servlets/client_secret.json";
   private static final Collection<String> SCOPES =
       Arrays.asList("https://www.googleapis.com/auth/youtube.readonly");
   private static final String APPLICATION_NAME = "YouTube Lounge";
@@ -57,12 +44,12 @@ public class SingleVideoSearch extends HttpServlet {
    */
   public YouTube getService() throws GeneralSecurityException, IOException {
     final YouTubeRequestInitializer KEY_INITIALIZER =
-      new YouTubeRequestInitializer("AIzaSyBIwlgpsYZLoI7cr347HyBoAETX9FvzXps");
+        new YouTubeRequestInitializer("AIzaSyBIwlgpsYZLoI7cr347HyBoAETX9FvzXps");
     final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
     return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
-      .setApplicationName("YouTube Lounge")
-      .setYouTubeRequestInitializer(KEY_INITIALIZER)
-      .build();
+        .setApplicationName("YouTube Lounge")
+        .setYouTubeRequestInitializer(KEY_INITIALIZER)
+        .build();
   }
 
   /**
@@ -73,7 +60,7 @@ public class SingleVideoSearch extends HttpServlet {
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    //accesses YouTube API to get a specific video as long as the given URL is valid
+    // accesses YouTube API to get a specific video as long as the given URL is valid
     Gson gson = new Gson();
     response.setContentType("application/json");
     String videoID = getParameter(request, "id", "");
@@ -120,8 +107,8 @@ public class SingleVideoSearch extends HttpServlet {
   }
 
   /**
-   * Iterates through the given items and locates specific values to 
-   * create a new Video and upload the video to DataStore
+   * Iterates through the given items and locates specific values to create a new Video and upload
+   * the video to DataStore
    */
   private void extractVideo(JsonArray items, String videoID) {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
@@ -133,12 +120,12 @@ public class SingleVideoSearch extends HttpServlet {
           snippet.getAsJsonObject("thumbnails").getAsJsonObject("medium").get("url").toString();
       String formattedVideoURL = "https://youtube.com/watch?v=" + videoID;
       String duration = 
-        items
-            .get(i)
-            .getAsJsonObject()
-            .getAsJsonObject("contentDetails")
-            .get("duration")
-            .toString();
+          items
+              .get(i)
+              .getAsJsonObject()
+              .getAsJsonObject("contentDetails")
+              .get("duration")
+              .toString();
       String channelName = snippet.get("channelTitle").toString();
       String releaseDate = snippet.get("publishedAt").toString();
       
@@ -179,8 +166,7 @@ public class SingleVideoSearch extends HttpServlet {
   }
 
   /**
-   * Locates the name parameter in the given request and
-   * returns that value.
+   * Locates the name parameter in the given request and returns that value.
    *
    * @return defaultValue if name parameter wasn't found
    * @return found value for name parameter
--- src/main/webapp/js/queue.js
+++ src/main/webapp/js/queue.js
@@ -1,3 +1,4 @@
+/*exported verifyURLStructure*/
 /*
   Makes sure the stub of the user's URL is valid and will warn user
   if their link wasn't formatted correctly for the server to parse.
@@ -5,7 +6,7 @@
 function verifyURLStructure(url) {
   if (!url.includes('https://www.youtube.com/watch?v=') || url.length <= 32) {
     console.log('false'); // signal bad url to user
-  } else if (document.getElementById('videoplayer') != null) { // already in a room
+  } else if (document.getElementById('videoplayer') != null) {
     console.log('good structure!');
     getVideoData(document.getElementById('linkArea').value.substring(32));
   } else { // starting a room
@@ -16,34 +17,34 @@ function verifyURLStructure(url) {
 
 // Retrieve video and create a new room via servlet GET request
 async function getVideoData(id) {
-  if (id == "") {
+  if (id == '') {
     return;
   }
   fetch('/vSearch?id=' + id)
       .then((response) => response.json())
       .then((video) => {
-            if (video.error != null) { // video was found, now load it into the website
-            // put thumbnail, title, duration, and channel name onto page
-            console.log(video);
-            } else { // video wasn't found, signal error to user
-              console.log(video.error);
-    }
-  });
+        if (video.error != null) { // video was found, now load
+          // put thumbnail, title, duration, and channel name onto page
+          console.log(video);
+        } else { // video wasn't found, signal error to user
+          console.log(video.error);
+        }
+      });
 }
 
 // Within a room, so just add video to room's queue via room ID
-async function getVideoData(id, room) {
+async function getVideoDataWithRoom(id, room) {
   if (id == '' || room == '') {
     return;
   }
   fetch('/vSearch?id=' + id + '&room=' + room)
-  .then((response) => response.json())
-  .then((video) => {
-    if (video.error != null) { // video was found, now load it into the website
-      // put thumbnail, title, duration, and channel name onto page
-      console.log(video);
-    } else { // video wasn't found, signal error to user
-      console.log(video.error);
-    }
-  });
+      .then((response) => response.json())
+      .then((video) => {
+        if (video.error != null) { // video was found, now load it into the website
+          // put thumbnail, title, duration, and channel name onto page
+          console.log(video);
+        } else { // video wasn't found, signal error to user
+          console.log(video.error);
+        }
+      });
 }

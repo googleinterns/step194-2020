9f60cf9bc83d182a0c29884a93591b7d7ed1c41c
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-07-23 17:28:17 +0000

    Linter fixes

--- src/main/webapp/index_script.js
+++ src/main/webapp/index_script.js
@@ -16,46 +16,46 @@ function openSidebar() { // eslint-disable-line no-unused-vars
   }
 }
 
-function removeVideo(element) {
-    console.log(element);
-    element.style.display = "none";
+function removeVideo(element) { // eslint-disable-line no-unused-vars
+  console.log(element);
+  element.style.display = 'none';
 }
 
-function addToQueue(url) {
-    document.getElementById("linkError").style.display = "none";
-    if (url == "https://www.youtube.com/watch?v=JhckVlgYZJE") {
-        document.getElementById("videoContainer").innerHTML += 
-            '<div id="video4" class="queueVideo">' +
-              '<img ' +
-                'class="videoThumbnail"' +
-                'src="images/oldman.png"' +
-              '/>' +
-              '<div id="video4Info" class="videoInfo"> ' +
-                '<p class="videoTitle">A old man\'s advice</p>' +
-                '<button class="removeVideoBtn" id="removeVideoBtn4" onclick="removeVideo(document.getElementById(\'video4\'))">' +
-                    '<img src="images/remove-from-queue.svg"/>' +
-                '</button>' +
-                '<p>10:20</p>' +
-              '</div>' +
-            '</div>';
-    }
-    else if (url == "https://www.youtube.com/watch?v=ZengOKCUBHo") {
-        document.getElementById("videoContainer").innerHTML += 
-            '<div id="video5" class="queueVideo">' +
-              '<img ' +
-                'class="videoThumbnail"' +
-                'src="images/juice.png"' +
-              '/>' +
-              '<div id="video5Info" class="videoInfo"> ' +
-                '<p class="videoTitle">Juice WRLD - Righteous (Official Video)</p>' +
-                '<button class="removeVideoBtn" id="removeVideoBtn5" onclick="removeVideo(document.getElementById(\'video5\'))">' +
-                    '<img src="images/remove-from-queue.svg"/>' +
-                '</button>' +
-                '<p>3:49</p>' +
-              '</div>' +
-            '</div>';
-    }
-    else {
-        document.getElementById("linkError").style.display = "block";
-    }
+function addToQueue(url) { // eslint-disable-line no-unused-vars
+  document.getElementById('linkError').style.display = 'none';
+  if (url == 'https://www.youtube.com/watch?v=JhckVlgYZJE') {
+    document.getElementById('videoContainer').innerHTML += 
+        '<div id="video4" class="queueVideo">' +
+          '<img ' +
+            'class="videoThumbnail"' +
+            'src="images/oldman.png"' +
+          '/>' +
+          '<div id="video4Info" class="videoInfo"> ' +
+            '<p class="videoTitle">A old man\'s advice</p>' +
+            '<button class="removeVideoBtn" id="removeVideoBtn4"' +
+              'onclick="removeVideo(document.getElementById(\'video4\'))">' +
+              '<img src="images/remove-from-queue.svg"/>' +
+            '</button>' +
+            '<p>10:20</p>' +
+          '</div>' +
+        '</div>';
+  } else if (url == 'https://www.youtube.com/watch?v=ZengOKCUBHo') {
+    document.getElementById('videoContainer').innerHTML +=
+    '<div id="video5" class="queueVideo">' +
+      '<img ' +
+        'class="videoThumbnail"' +
+        'src="images/juice.png"' +
+      '/>' +
+      '<div id="video5Info" class="videoInfo"> ' +
+        '<p class="videoTitle">Juice WRLD - Righteous (Official Video)</p>' +
+        '<button class="removeVideoBtn" id="removeVideoBtn5"' +
+        'onclick="removeVideo(document.getElementById(\'video5\'))">' +
+            '<img src="images/remove-from-queue.svg"/>' +
+        '</button>' +
+        '<p>3:49</p>' +
+      '</div>' +
+    '</div>';
+  } else {
+    document.getElementById('linkError').style.display = 'block';
+  }
 }

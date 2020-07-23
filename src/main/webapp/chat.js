4472d0c5770a0bd850a8ccdd35ab975f667f829c
Caroline <carolineehana@gmail.com>
2020-07-23 17:06:16 +0000

    Linter fixes

--- src/main/webapp/chat.js
+++ src/main/webapp/chat.js
@@ -1,43 +1,44 @@
-// local storage of comments
-window.onload = function(){
-var input = document.getElementById("new-message");
-document.getElementById("send").onclick = function() {input.forEach(addChatMessage())};
 
-input.addEventListener("keyup", function(event) {
+window.onload = function() {
+  const input = document.getElementById('new-message');
+  document.getElementById('send').onclick = function() {
+      input.forEach(addChatMessage())};
+
+  input.addEventListener('keyup', function(event) {
   // "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-        input.forEach(addChatMessage());
-  }
-});
-var formattedComment = "";
-
-function addChatMessage() {
-    var message = document.getElementById("new-message");
-     const chatList = document.getElementById('chat-history');
-
-    if(message !== ""){
-        formattedComment = createChatElement(message);
-        chatList.appendChild(formattedComment);
-        document.getElementById("new-message").value = "";
-        }
+    if (event.keyCode === 13) {
+      input.forEach(addChatMessage());
     }
- }
+  });
+  let formattedComment = '';
 
-//placeholder items for avatar and display name
- function createChatElement(message) {
-    const avatarElement = document.createElement('img');
-    avatarElement.src= "https://cdn.iconscout.com/icon/free/png-512/avatar-372-456324.png";
-    avatarElement.className = "avatar";
+  function addChatMessage() {
+    const message = document.getElementById('new-message');
+    const chatList = document.getElementById('chat-history');
 
-    const textElement = document.createElement('span');
-    textElement.innerText = message.value + "\n";
+    if (message !== ''){
+      formattedComment = createChatElement(message);
+      chatList.appendChild(formattedComment);
+      document.getElementById('new-message').value = '';
+    }
+  }
+};
 
-    const UserInfoElement = document.createElement('span');
-    UserInfoElement.innerHTML = "  <b>Sundar Pichai</b>  ";
+// placeholder items for avatar and display name
+function createChatElement(message) {
+  const avatarElement = document.createElement('img');
+  avatarElement.src= 'https://cdn.iconscout.com/icon/free/png-512/avatar-372-456324.png';
+  avatarElement.className = 'avatar';
+
+  const textElement = document.createElement('span');
+  textElement.innerText = message.value + "\n";
+
+  const UserInfoElement = document.createElement('span');
+  UserInfoElement.innerHTML = '  <b>Sundar Pichai</b>  ';
     
-    const chatListElement = document.createElement('li');
-    chatListElement.appendChild(avatarElement);
-    chatListElement.appendChild(UserInfoElement);
-    chatListElement.appendChild(textElement);
-    return chatListElement;
+  const chatListElement = document.createElement('li');
+  chatListElement.appendChild(avatarElement);
+  chatListElement.appendChild(UserInfoElement);
+  chatListElement.appendChild(textElement);
+  return chatListElement;
 }

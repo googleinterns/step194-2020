a038dce013ed175967dc06f24582b63af4858d9e
Clayton <claytonferg01@gmail.com>
2020-07-24 16:07:14 +0000

    Linter fixes

--- src/main/java/com/google/step/YTLounge/servlets/PlaybackCalls.java
+++ src/main/java/com/google/step/YTLounge/servlets/PlaybackCalls.java
@@ -14,11 +14,10 @@
 
 package com.google.step.YTLounge.servlets;
 
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.Entity;
 import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;

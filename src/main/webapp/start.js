1515ea4d5243844bc6f09262b7e02d398f324122
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-08-03 18:43:16 +0000

    Linter fixes

--- src/main/webapp/start.js
+++ src/main/webapp/start.js
@@ -1,8 +1,9 @@
-/** exported startRoom */
+/* exported startRoom */
+// Takes the user to the lounge page when the start button is pressed
 async function startRoom() {
   fetch('/startRoom')
       .then((response) => response.json())
       .then((responseJson) => {
-          console.log("Switching to lounge: " + responseJson);
+        console.log("Switching to lounge: " + responseJson);
       });
 }

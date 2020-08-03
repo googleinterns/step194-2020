e4d6bf57839ea63dd50bc67d5add2d80216ea590
Caroline <carolineehana@gmail.com>
2020-08-03 19:20:52 +0000

    Add media files, style fixes.

--- src/main/webapp/index_script.js
+++ src/main/webapp/index_script.js
@@ -3,14 +3,14 @@ function openSidebar() { // eslint-disable-line no-unused-vars
   const buttonText = document.getElementById('sidebarBtn');
 
   if (buttonText.innerHTML ===
-    '<span class="glyphicon glyphicon-menu-left"></span>') {
+    '<span class="material-icons">chevron_left</span>') {
     buttonText.innerHTML =
-    '<span class="glyphicon glyphicon-menu-right"></span>';
+    '<span class="material-icons">chevron_right</span>';
     document.getElementById('mySidebar').style.width = '25%';
     document.getElementById('main').style.marginRight = '25%';
   } else {
     buttonText.innerHTML =
-    '<span class="glyphicon glyphicon-menu-left"></span>';
+    '<span class="material-icons">chevron_left</span>';
     document.getElementById('mySidebar').style.width = '0';
     document.getElementById('main').style.marginRight= '0';
   }

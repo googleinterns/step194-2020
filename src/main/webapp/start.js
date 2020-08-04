4949afea8cf448ca62ff196c1ed387f929bd5ded
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-08-04 18:44:49 +0000

    Linter fixes

--- src/main/webapp/start.js
+++ src/main/webapp/start.js
@@ -16,12 +16,12 @@ db = firebase.firestore(app); // eslint-disable-line no-undef
 /* exported startRoom */
 // Takes the user to the lounge page when the start button is pressed
 async function startRoom() {
-  db.collection('rooms').add({
-      duration: '0',
-      elapsedTime: '0'
+  db.collection('rooms').add({ // eslint-disable-line no-undef
+    duration: '0',
+    elapsedTime: '0',
   })
-  .then(function(docRef) {
-      window.location.href = 'lounge.html/?room_id=' + docRef.id;
-  });
+      .then(function(docRef) {
+        window.location.href = 'lounge.html/?room_id=' + docRef.id;
+      });
   setTimeout(5000);
 }

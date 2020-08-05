73bb55924a3740ccd1c8b960be59e45678ab3f6c
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-08-05 15:35:06 +0000

    Adjust page sources and start button behaviors

--- src/main/webapp/start.js
+++ src/main/webapp/start.js
@@ -12,16 +12,18 @@ const firebaseConfig = {
 const app =
     firebase.initializeApp(firebaseConfig); // eslint-disable-line no-undef
 db = firebase.firestore(app); // eslint-disable-line no-undef
-
+let startButtonCount = 0;
 /* exported startRoom */
 // Takes the user to the lounge page when the start button is pressed
 async function startRoom() {
-  db.collection('rooms').add({ // eslint-disable-line no-undef
-    duration: '0',
-    elapsedTime: '0',
-  })
+  if (startButtonCount < 1) {
+    db.collection('rooms').add({ // eslint-disable-line no-undef
+      duration: '0',
+      elapsedTime: '0',
+    })
       .then(function(docRef) {
         window.location.href = 'lounge.html/?room_id=' + docRef.id;
       });
-  setTimeout(5000);
+    startButtonCount++;
+  }
 }

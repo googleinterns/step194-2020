30e36cb0ad613e3a2702ad73ac0f6f48dc685364
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-08-05 15:40:36 +0000

    Linter fixes

--- src/main/webapp/start.js
+++ src/main/webapp/start.js
@@ -1,10 +1,10 @@
 // Initializes resources for reading/writing to Firestore
 const firebaseConfig = {
   apiKey: config.apiKey, // eslint-disable-line no-undef
-  authDomain: 'lounge-95f01.firebaseapp.com',
-  databaseURL: 'https://lounge-95f01.firebaseio.com',
-  projectId: 'youtube-lounge',
-  storageBucket: 'youtube-lounge.appspot.com',
+  authDomain: config.authDomain, // eslint-disable-line no-undef
+  databaseURL: config.databaseURL, // eslint-disable-line no-undef
+  projectId: config.projectId, // eslint-disable-line no-undef
+  storageBucket: config.storageBucket, // eslint-disable-line no-undef
   messagingSenderId: config.messagingSenderId, // eslint-disable-line no-undef
   appId: config.appId, // eslint-disable-line no-undef
   measurementId: config.measurementId, // eslint-disable-line no-undef
@@ -21,9 +21,9 @@ async function startRoom() {
       duration: '0',
       elapsedTime: '0',
     })
-      .then(function(docRef) {
-        window.location.href = 'lounge.html/?room_id=' + docRef.id;
-      });
+        .then(function(docRef) {
+          window.location.href = 'lounge.html/?room_id=' + docRef.id;
+        });
     startButtonCount++;
   }
 }

0d7f63916a72c9c25701755c468f0f30cbdbee63
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-08-11 18:38:22 +0000

    Add tab to sidebar for users to share lounge link

--- src/main/webapp/index_script.js
+++ src/main/webapp/index_script.js
@@ -15,3 +15,18 @@ function openSidebar() { // eslint-disable-line no-unused-vars
     document.getElementById('main').style.marginRight= '0';
   }
 }
+
+function copyLink() {
+  /* Get the text field */
+  var copyText = document.getElementById("loungeLink");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}

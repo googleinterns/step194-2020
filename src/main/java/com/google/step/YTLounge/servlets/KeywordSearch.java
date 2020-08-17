5a732bb222158508d9f802ad3d342af995b92d9c
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-08-17 20:44:54 +0000

    Linter fixes

--- src/main/java/com/google/step/YTLounge/servlets/KeywordSearch.java
+++ src/main/java/com/google/step/YTLounge/servlets/KeywordSearch.java
@@ -4,7 +4,6 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.core.ApiFuture;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.YouTubeRequestInitializer;
 import com.google.api.services.youtube.model.SearchListResponse;

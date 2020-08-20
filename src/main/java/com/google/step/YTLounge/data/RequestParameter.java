69f210a17532354d19d46153e6b92a3cbbc2aa08
Noah Krohngold <Noah.Krohngold@gmail.com>
2020-08-20 17:20:44 +0000

    Linter fix

--- src/main/java/com/google/step/YTLounge/data/RequestParameter.java
+++ src/main/java/com/google/step/YTLounge/data/RequestParameter.java
@@ -11,8 +11,8 @@ public class RequestParameter {
    * @param request the request made to the servlet
    * @param name the name of the desired request parameter
    * @param defaultValue the base value to return if the desired parameter isn't in the request
-   * @return the value of the desired request parameter if found in the request, otherwise
-   * the given defaultValue
+   * @return the value of the desired request parameter if found in the request, otherwise the given
+   *     defaultValue
    */
   public static String getParameter(HttpServletRequest request, String name, String defaultValue) {
     String value = request.getParameter(name);

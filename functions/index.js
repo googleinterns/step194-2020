60083ba8d38fce1f9af7e2d5a80e1376bd74e9b2
Caroline <carolineehana@gmail.com>
2020-08-24 13:00:58 +0000

    Chnage document path for onupdate method.

--- functions/index.js
+++ functions/index.js
@@ -6,13 +6,13 @@ admin.initializeApp();
 
 
 // Adds a message that welcomes new users into the chat.
-exports.addWelcomeMessages = functions.auth.user().onCreate(async (user, docRef) => {
+exports.addWelcomeMessages = functions.auth.user().onCreate(async (user) => {
   console.log('A new user signed in for the first time.');
   const fullName = user.name || 'Anonymous';
 
   // Saves the new welcome message into the database
   // which then displays it in the FriendlyChat clients.
-  await admin.firestore().collection('rooms').doc(docRef.id)
+  await admin.firestore().collection('rooms').doc(roomId)
       .collection('messages').add({
     name: 'Lounge Bot',
     profilePicUrl: '/images/LoungeLogo.png',
@@ -22,7 +22,7 @@ exports.addWelcomeMessages = functions.auth.user().onCreate(async (user, docRef)
   console.log('Welcome message written to database.');
 });
 
-exports.addLeaveMessages = functions.auth.user().onDelete(async (user, docRef) => {
+exports.addLeaveMessages = functions.auth.user().onDelete(async (user) => {
   console.log('A new user signed in for the first time.');
   const fullName = user.name || 'Anonymous';
 
@@ -39,21 +39,21 @@ exports.addLeaveMessages = functions.auth.user().onDelete(async (user, docRef) =
 });
 
 
-exports.updatePlayBack = functions.firestore.document('rooms/{room_id}/CurrentVideo/{PlaybackData}').onUpdate((change, context) => {
+exports.updatePlayBack = functions.firestore.document('rooms/{roomId}/CurrentVideo/{PlaybackData}').onUpdate((change, context) => {
       // Get an object representing the document
       const newValue = change.after.data();
 
       // access a particular field 
       const isPlaying = newValue.isPlaying;
         if (isPlaying == true) {
-            admin.firestore().collection('rooms').doc(docRef.id).collection('messages').add({
+            admin.firestore().collection('rooms').doc(roomId).collection('messages').add({
             name: 'Lounge Bot',
             profilePicUrl: '/images/LoungeLogo.png',
             text: `Video is playing`,
             timestamp: admin.firestore.FieldValue.serverTimestamp(),
             });
         } else {
-           admin.firestore().collection('rooms').doc(room.docRef.id).collection('messages').add({
+           admin.firestore().collection('rooms').doc(roomId).collection('messages').add({
             name: 'Lounge Bot',
             profilePicUrl: '/images/LoungeLogo.png',
             text: `Video is paused`,
